fbb3c6b9c2283f17080922647df8692708394fb1
GregThompsonMN <GregThompsonMN@gmail.com>
2015-06-04 23:24:04 -0500

    Added Body and Face Readers, face tracking

--- BabyMonitor/MainWindow.xaml.cs
+++ BabyMonitor/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
 
 namespace BabyMonitor
 {
@@ -85,6 +86,32 @@ namespace BabyMonitor
         private ColorFrameReader colorFrameReader = null;
 
         private BodyFrameReader bodyFrameReader = null;
+        /// <summary>
+        /// Array to store bodies
+        /// </summary>
+        private Body[] bodies = null;
+
+        /// <summary>
+        /// Number of bodies tracked
+        /// </summary>
+        private int bodyCount;
+
+        /// <summary>
+        /// Face frame sources
+        /// </summary>
+        private FaceFrameSource[] faceFrameSources = null;
+
+        /// <summary>
+        /// Face frame readers
+        /// </summary>
+        private FaceFrameReader[] faceFrameReaders = null;
+
+        /// <summary>
+        /// Storage for face frame results
+        /// </summary>
+        private FaceFrameResult[] faceFrameResults = null;
+
+        private Rect? LastFaceLocation = null;
 
         /// <summary>
         /// Bitmap to display
@@ -139,8 +166,44 @@ namespace BabyMonitor
 
             this.bodyFrameReader.FrameArrived += bodyFrameReader_FrameArrived;
 
+            // set the maximum number of bodies that would be tracked by Kinect
+            this.bodyCount = this.kinectSensor.BodyFrameSource.BodyCount;
+
+            // allocate storage to store body objects
+            this.bodies = new Body[this.bodyCount];
+
+            // specify the required face frame results
+            FaceFrameFeatures faceFrameFeatures =
+                FaceFrameFeatures.BoundingBoxInColorSpace
+                | FaceFrameFeatures.PointsInColorSpace
+                | FaceFrameFeatures.RotationOrientation
+                | FaceFrameFeatures.FaceEngagement
+                | FaceFrameFeatures.Glasses
+                | FaceFrameFeatures.Happy
+                | FaceFrameFeatures.LeftEyeClosed
+                | FaceFrameFeatures.RightEyeClosed
+                | FaceFrameFeatures.LookingAway
+                | FaceFrameFeatures.MouthMoved
+                | FaceFrameFeatures.MouthOpen;
+
+            // create a face frame source + reader to track each face in the FOV
+            this.faceFrameSources = new FaceFrameSource[this.bodyCount];
+            this.faceFrameReaders = new FaceFrameReader[this.bodyCount];
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                // create the face frame source with the required face frame features and an initial tracking Id of 0
+                this.faceFrameSources[i] = new FaceFrameSource(this.kinectSensor, 0, faceFrameFeatures);
+
+                // open the corresponding reader
+                this.faceFrameReaders[i] = this.faceFrameSources[i].OpenReader();
+            }
+
+            // allocate storage to store face frame results for each face in the FOV
+            this.faceFrameResults = new FaceFrameResult[this.bodyCount];
+
             // Set up coordinate mapper
-            //this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+            
 
             // Get the color Frame info, and create a bitmap of that size
             FrameDescription colorFrameDescription = this.kinectSensor.ColorFrameSource.FrameDescription;
@@ -170,7 +233,45 @@ namespace BabyMonitor
 
         void bodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
         {
-            //throw new NotImplementedException();
+            LastFaceLocation = null;
+            using (var bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+
+                    for (int i = 0; i < this.bodyCount; i++)
+                    {
+                        if (this.faceFrameSources[i].IsTrackingIdValid)
+                        {
+                            if (this.faceFrameResults[i] != null)
+                            {
+                                if (ShowDay)
+                                {
+                                    var box = faceFrameResults[i].FaceBoundingBoxInColorSpace;
+                                    LastFaceLocation = new Rect(new Point(box.Left, box.Bottom), new Point(box.Right, box.Top));
+                                    Console.WriteLine("Drawing face: " + LastFaceLocation.ToString());
+                                }
+                                else
+                                {
+                                    var box = faceFrameResults[i].FaceBoundingBoxInInfraredSpace;
+                                    LastFaceLocation = new Rect(new Point(box.Left, box.Bottom), new Point(box.Right, box.Top));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // check if the corresponding body is tracked 
+                            if (this.bodies[i].IsTracked)
+                            {
+                                // update the face frame source to track this body
+                                this.faceFrameSources[i].TrackingId = this.bodies[i].TrackingId;
+                            }
+                        }
+                    }
+                }
+            }
+
         }
 
         void colorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
@@ -254,7 +355,14 @@ namespace BabyMonitor
                     colorFrame.CopyConvertedFrameDataToIntPtr(this.bitmap.BackBuffer, this.bitmapBackBufferSize, ColorImageFormat.Bgra);
                     colorFrame.Dispose();
                     colorFrame = null;
-                    this.bitmap.AddDirtyRect(new Int32Rect(0, 0, this.bitmap.PixelWidth, this.bitmap.PixelHeight));
+                    if (LastFaceLocation == null)
+                    {
+                        this.bitmap.AddDirtyRect(new Int32Rect(0, 0, this.bitmap.PixelWidth, this.bitmap.PixelHeight));
+                    }
+                    else
+                    {
+                        this.bitmap.AddDirtyRect(new Int32Rect((int)LastFaceLocation.Value.BottomLeft.X, (int)LastFaceLocation.Value.BottomLeft.Y, (int)LastFaceLocation.Value.Width, (int)LastFaceLocation.Value.Height));
+                    }
                 }
                 else
                 {
@@ -362,7 +470,7 @@ namespace BabyMonitor
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //ShowDay = true;
+            ShowDay = true;
             DayPic.Visibility = Visibility.Visible;
             NightPic.Visibility = Visibility.Hidden;
             //this.bitmap = new WriteableBitmap(1920, 1080, 96.0, 96.0, PixelFormats.Bgra32, null);
@@ -370,11 +478,110 @@ namespace BabyMonitor
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            //ShowDay = false;
+            ShowDay = false;
             DayPic.Visibility = Visibility.Hidden;
             NightPic.Visibility = Visibility.Visible;
             //this.bitmap = new WriteableBitmap(512, 424, 96.0, 96.0, PixelFormats.Bgra32, null);
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                if (this.faceFrameReaders[i] != null)
+                {
+                    // wire handler for face frame arrival
+                    this.faceFrameReaders[i].FrameArrived += MainWindow_FrameArrived;
+                }
+            }
+
+            if (this.bodyFrameReader != null)
+            {
+                // wire handler for body frame arrival
+                this.bodyFrameReader.FrameArrived += bodyFrameReader_FrameArrived;
+            }
+        }
+
+        void MainWindow_FrameArrived(object sender, FaceFrameArrivedEventArgs e)
+        {
+            using (FaceFrame faceFrame = e.FrameReference.AcquireFrame())
+            {
+                if (faceFrame != null)
+                {
+                    // get the index of the face source from the face source array
+                    int index = this.GetFaceSourceIndex(faceFrame.FaceFrameSource);
+
+                    // check if this face frame has valid face frame results
+                    if (this.ValidateFaceBoxAndPoints(faceFrame.FaceFrameResult))
+                    {
+                        // store this face frame result to draw later
+                        this.faceFrameResults[index] = faceFrame.FaceFrameResult;
+                    }
+                    else
+                    {
+                        // indicates that the latest face frame result from this reader is invalid
+                        this.faceFrameResults[index] = null;
+                    }
+                }
+            }
+        }
+
+        private int GetFaceSourceIndex(FaceFrameSource faceFrameSource)
+        {
+            int index = -1;
+
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                if (this.faceFrameSources[i] == faceFrameSource)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        private bool ValidateFaceBoxAndPoints(FaceFrameResult faceResult)
+        {
+            bool isFaceValid = faceResult != null;
+
+            if (isFaceValid)
+            {
+                var faceBox = faceResult.FaceBoundingBoxInColorSpace;
+                if (faceBox != null)
+                {
+                    // check if we have a valid rectangle within the bounds of the screen space
+                    isFaceValid = (faceBox.Right - faceBox.Left) > 0 &&
+                                  (faceBox.Bottom - faceBox.Top) > 0 &&
+                                  faceBox.Right <= 1980 &&
+                                  faceBox.Bottom <= 1020;
+                    // used to be this.displayWidth and this.displayHeight
+                    if (isFaceValid)
+                    {
+                        var facePoints = faceResult.FacePointsInColorSpace;
+                        if (facePoints != null)
+                        {
+                            foreach (PointF pointF in facePoints.Values)
+                            {
+                                // check if we have a valid face point within the bounds of the screen space
+                                bool isFacePointValid = pointF.X > 0.0f &&
+                                                        pointF.Y > 0.0f &&
+                                                        pointF.X < 1980 &&
+                                                        pointF.Y < 1020;
+
+                                if (!isFacePointValid)
+                                {
+                                    isFaceValid = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return isFaceValid;
+        }
     }
 }
